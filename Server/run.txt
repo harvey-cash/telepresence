f40defa8a6ded53820bbaa77e7ad96dc8afb55c7
Harvey Cash <HCash1@sheffield.ac.uk>
2019-03-07 12:35:01 +0000

    Remove run.txt

--- Server/run.txt
+++
@@ -1,1 +0,0 @@
-python ./stitch_server.py
--- Server/stitch_server.py
+++ Server/stitch_server.py
@@ -5,6 +5,8 @@
 
 import time
 import zmq
+# import cv2
+# import numpy as np
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -15,12 +17,12 @@ print("~~ I'm doing good work ! ~~")
 print("~~~~~~~   <(-_-)>   ~~~~~~~")
 
 while True:
-
     #  Wait for next request from client
     message = socket.recv()
 
-    #  Do some 'work'.
-    # time.sleep(1)
+    # Split message into left and right images
+
+    # Process
 
     #  Send reply back to client
     socket.send(message)
