d94dd6ec222a84a52b8eff7de274419f5691b593
Harvey Cash <HCash1@sheffield.ac.uk>
2019-03-08 15:54:05 +0000

    Combine camera_streamer into stitch_server

--- Server/stitch_server.py
+++ Server/stitch_server.py
@@ -1,33 +1,106 @@
-#
-#   Stitching Server
-#   Binds REP socket to tcp://*:5555
-#
+#!/usr/bin/python
 
 import time
-import zmq
+import os
+
+import rospy
+from std_msgs.msg import Float32MultiArray, UInt32MultiArray, UInt16MultiArray, UInt8MultiArray, UInt16, Int16MultiArray, String
+from geometry_msgs.msg import TwistStamped
+from sensor_msgs.msg import JointState, BatteryState, Imu, Range, CompressedImage
+
 import cv2
+from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+import math
+import miro2 as miro
 
-print("~~ STITCHING SERVER V1.0 ~~")
-print("~~ I'm doing good work ! ~~")
-print("~~~~~~~   <(-_-)>   ~~~~~~~")
+import zmq
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv(0, True)
-    b = bytearray()
-    b.extend(message)
+################################################################
+## MAIN
 
-    # Split message into left and right images
+if __name__ == "__main__":
+	main = cam_stream()
+	rospy.init_node("cam_stream")
 
-    # Process
-    img = cv2.imdecode(np.array(b), 1);
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
 
-    #  Send reply back to client
-    # ENCODE TO JPG BYTE STR FOR UNITY
-    stitched = cv2.imencode('.jpg', img)[1].tostring()
-    socket.send(stitched)
+    print("~~ STITCHING SERVER V1.1 ~~")
+    print("~~ I'm doing good work ! ~~")
+    print("~~~~~~~   <(n_n)>   ~~~~~~~")
+
+    while True:
+        #  Wait for next request from client
+        message = socket.recv(0, True)
+        # b = bytearray()
+        # b.extend(message)
+
+        # Split message into left and right images
+        # img = cv2.imdecode(np.array(b), 1);
+        left = main.get_image(0)
+        right = main.get_image(1)
+
+        # Process
+        img = left # temporary
+
+        #  Send reply back to client
+        # ENCODE TO JPG BYTE STR FOR UNITY
+        stitched = cv2.imencode('.jpg', img)[1].tostring()
+        socket.send(stitched)
+
+class cam_stream:
+
+	def __init__(self):
+		# variables to store input data
+		self.input_camera = [None, None]
+		self.t_input_camera = [[], []]
+
+		self.camera_zoom = None
+		self.auto_camera_zoom = [0, 0] # determine zoom from first received frame
+		self.frame_params = [180, '180w', 15]
+		self.meas_fps = ["", ""]
+
+		#Create object to convert ROS images to OpenCV format
+		self.image_converter = CvBridge()
+
+		# robot name
+		topic_base = "/" + os.getenv("MIRO_ROBOT_NAME") + "/"
+
+		# subsribe to cameras
+		self.sub_caml = rospy.Subscriber(topic_base + "sensors/caml",
+							CompressedImage, self.callback_caml)
+		self.sub_camr = rospy.Subscriber(topic_base + "sensors/camr",
+							CompressedImage, self.callback_camr)
+
+
+    # GET IMAGE!
+	def get_image(self, i):
+		return self.input_camera[i]
+
+
+	def callback_caml(self, ros_image):
+
+		self.callback_cam(ros_image, 0)
+
+	def callback_camr(self, ros_image):
+
+		self.callback_cam(ros_image, 1)
+
+	def callback_cam(self, ros_image, index):
+			# silently (ish) handle corrupted JPEG frames
+			try:
+				# convert compressed ROS image to raw CV image
+				image = self.image_converter.compressed_imgmsg_to_cv2(ros_image, "rgb8")
+
+				# store image for display
+				self.input_camera[index] = image
+				cv2.imwrite("./img.jpg", image)
+
+			except CvBridgeError as e:
+
+				# swallow error, silently
+				#print(e)
+				pass
