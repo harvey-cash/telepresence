22dfd7bae2437ee533700a370ecf4bb48c470152
Harvey Cash <HCash1@sheffield.ac.uk>
2019-05-20 15:18:04 +0100

    Demo Build

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# telepresence
-3rd Year Undergraduate Dissertation
+# Improving the Visual Comfort of Virtual Reality Telepresence
+
+Telepresence technologies enable users to exhibit a presence in a remote location, through the use of sensors, networks and robotics. State of the art telepresence research swaps conventional desktop monitors for Virtual Reality (VR) headsets, in order to increase the user's immersion in the remote environment, often at the cost of increased nausea and oculomotor discomfort. 
+
+
+
+This project aimed to improve the comfort of VR telepresence by accounting for discrepancies between robot and user head pose through design and development of a ``Decoupled'' image projection technique, whereby the user is able to look across captured imagery rendered to a virtual display plane.
+
+
+
+Evaluated against conventional techniques through a study on 19 participants, Decoupled image projection substantially reduced mean perceived nausea and oculomotor discomfort within a 95% statistical confidence interval, while also improving task performance, immersiveness, and the perceived sensation of presence.
---
+++ TelepresenceProject/Assets/Scripts/User/User.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class User : MonoBehaviour {
+    public static User user;
+    
+    [SerializeField]
+    public MODE mode;
+
+    public VirtualDisplay display;
+    public Viewer viewer;
+
+    private Vector3 storeDisplayPos = Vector3.zero;
+    public void SetStoreDisplayPos(Vector3 pos) { clampHeadPosition = false; storeDisplayPos = pos; }
+
+    bool clampHeadPosition = true;
+
+    private void Awake() {
+        user = this;
+        Debug.LogWarning("MODE IS: " + mode.ToString());
+
+        // Join the network
+        Network.Join(this);
+    }
+
+    private void Start() {
+        viewer = Network.Viewer;
+        display = Network.Display;
+        display.SetUser(this);
+
+        // Post head pose
+        InvokeRepeating("PostHeadPose", 0, Config.POST_HEAD_POSE_MS / 1000f);
+
+        // Pass callback for viewer to set display pos, after a few seconds wait
+        if (!Config.CENTER_DISPLAY_ON_HEAD) {
+            storeDisplayPos = viewer.GetHeadPose().position;
+            viewer.SetDisplayPosition(SetStoreDisplayPos);
+        }
+    }
+
+    private void Update() {
+
+        if (Input.GetKeyUp(KeyCode.Alpha1)) {            
+            mode = MODE.COUPLE_SLOW;
+            display.ResetMode();
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2)) {            
+            mode = MODE.DECOUPLE_SLOW;
+            display.ResetMode();
+        }
+
+        /*
+        float left = Input.GetAxis("Vertical");
+        float right = Input.GetAxis("RightStick");
+        System.Action<float, float> target = Network.Robot.WheelVel;
+
+        StartCoroutine(Network.Post(target, left, right));
+        */
+    }
+
+    // Send head pose over the Network
+    private void PostHeadPose() {
+        Pose headPose = viewer.GetHeadPose();
+        System.Action<float, Pose> target = Network.Robot.ReceiveHeadPose;
+
+        StartCoroutine(Network.Post(target, Time.time / 1000f, headPose));
+    }
+
+    // Stitched Imagery is passed to the display for rendering
+    public void ReceiveImageryAndPose(float timestamp, RenderTexture renderTexture, Pose pose) {
+        // Center display on user?
+        if (!Config.CENTER_DISPLAY_ON_HEAD && !clampHeadPosition) {
+            // TODO: Account for robot head position change here?
+            pose = new Pose(storeDisplayPos, pose.rotation);
+        }
+        else {
+            pose = new Pose(viewer.GetHeadPose().position, pose.rotation);
+        }
+
+        display.ReceiveImageryAndPose(timestamp, renderTexture, pose);
+    }
+
+    // Stitched Imagery is passed to the display for rendering
+    public void ReceiveImageryAndPose(float timestamp, byte[] imagery, Pose pose) {
+        // Center display on user?
+        if (!Config.CENTER_DISPLAY_ON_HEAD && !clampHeadPosition) {
+            pose = new Pose(storeDisplayPos, pose.rotation);
+        }
+        else {
+            pose = new Pose(viewer.GetHeadPose().position, pose.rotation);
+        }
+
+        throw new Exception("No implementation for byte[] imagery anymore!");
+    }
+}
